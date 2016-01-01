ddd50b800d1ddfef2d33eebc9c1702a43ecbfeac
Alex Pendleton <alexcpendleton@gmail.com>
2015-12-31 21:45:18 -0500

    Made the initial project, and also the pod file with tesseract in it
    Contains no actual code changes in the project

---
+++ ios/source/MonsterHunterTalismanScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonsterHunterTalismanScanner
+//
+//  Created by Alex Pendleton on 12/31/15.
+//  Copyright © 2015 Alex Pendleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
