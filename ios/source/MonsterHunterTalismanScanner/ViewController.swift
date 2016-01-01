0e070f9ea74d0392acb6f82591c00adff8bfeff2
Alex Pendleton <alexcpendleton@gmail.com>
2016-01-01 13:13:16 -0500

    Tried using bazaar, don't know if it's done anything...

--- ios/source/MonsterHunterTalismanScanner/ViewController.swift
+++ ios/source/MonsterHunterTalismanScanner/ViewController.swift
@@ -31,8 +31,8 @@ class ViewController: UIViewController {
     
     func run() {
         let imageName =
-            "SamplePhotos/SingleTalismanInverted03.JPG"
-//            "SamplePhotos/CroppedTalismans03 copy.JPG"
+        //"SamplePhotos/SingleTalismanInverted03.JPG"
+            "SamplePhotos/CroppedTalismans03 copy.JPG"
         let originalImage = UIImage(named: imageName)
         let scaledSource = scaleImage(originalImage!, maxDimension: 640)
 
@@ -65,7 +65,7 @@ class ViewController: UIViewController {
     
     func performImageRecognition(image: UIImage) {
         // 1
-        let tesseract = G8Tesseract(language: "mhxjpn")
+        let tesseract = G8Tesseract(language: "mhxjpn", configDictionary: nil, configFileNames: ["bazaar"], absoluteDataPath: nil, engineMode: .TesseractOnly, copyFilesFromResources: true)
         
         // 2
         //tesseract.language = "jpn"
@@ -83,13 +83,26 @@ class ViewController: UIViewController {
         // 6
         //UIGraphicsBeginImageContextWithOptions(bounds, NO, 1.0f)
 
-        //let img = invertImage(image.g8_blackAndWhite())
-        let img = image.g8_blackAndWhite()
+        var img = image //invertImage(image)
+        img = img.g8_blackAndWhite()
         tesseract.image = img
 
         let uniqueChars = Set<String>(arrayLiteral: "離笛覚調心れ採睡令里動加刀観ユ宝沫追適火広地射化ナKト2学対本盾上雷矛采黒砕龍度闘風撃N効力設細泡鈍則御重法野いめま1剣復金逆斬能刃窮ル5眼磨氷燼石取の岩爆盗躍熱捕術拡纏紫ガ白散疾3痺0ー搬圧無置ツ9節匠聴り瓶86殊タ納気坊ド水会精O師知肉填数炎避狂跳だミハ短榴弾回ん耐識研ポ運ぐ強功絶通痛紅獲姫茸公傷W約率護ベ+コ剥隻毒果破菌D装ケモ裂眠常速攻人ク鉤草接7体大スッ千成特腹麻雪応狩滅薬寒冷号震味配穿王察食ぎ量み暑減る続根底砲変荒性乗兜雄達縮主持属ャ高防抜ン保連事魂域境チ反英貫し距4合爪溜密器")
-        tesseract.charWhitelist = uniqueChars.joinWithSeparator("")
+        //tesseract.charWhitelist = uniqueChars.joinWithSeparator("")
 
+        /*
+
+instance.setTessVariable("load_system_dawg", "F");
+instance.setTessVariable("load_freq_dawg", "F");
+instance.setTessVariable("user_words_suffix", "user-words");
+*/
+        /*
+        tesseract.setVariableValue("F", forKey: "load_system_dawg")
+        tesseract.setVariableValue("F", forKey: "load_freq_dawg")
+        tesseract.setVariableValue("user-words", forKey: "user_words_suffix")
+        */
+        
+        
         tesseract.recognize()
         
         // 7
